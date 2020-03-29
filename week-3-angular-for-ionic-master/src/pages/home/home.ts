c497d13caa7d4af1847b0ad94cfc813d56b30a3e
Elderied McKinney <mckinneydemetrice@icloud.com>
2020-03-29 18:54:50 -0500

    update test2

---
+++ week-3-angular-for-ionic-master/src/pages/home/home.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  title = "Grocery";
+
+  items = [
+    {
+      name: "Milk",
+      quantity: 2    
+    },
+    {
+      name: "Bread",
+      quantity: 1    
+    },
+    {
+      name: "Banana",
+      quantity: 3    
+    },
+    {
+      name: "Sugar",
+      quantity: 1    
+    },
+  ];
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+
+  }
+
+  removeItem(item) {
+    console.log("Removing Item - ", item);
+    const toast = this.toastCtrl.create({
+      message: 'Removing Item - ' + item.name + " ...",
+      duration: 3000
+    });
+    toast.present();
+  }
+
+}
